4c555967195c560406c90f6e96ce53538cddd524
Fachrurazzi <razzytirta@gmail.com>
2024-12-17 13:26:27 +0700

    Setup API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/products', [ProductController::class, 'all']);
+Route::get('/categories', [ProductCategoryController::class, 'all']);
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [UserController::class, 'fetch']);
+    Route::post('/user', [UserController::class, 'updateProfile']);
+    Route::post('/logout', [UserController::class, 'logout']);
+
+    Route::get('/transaction', [TransactionController::class, 'all']);
+    Route::get('/checkout', [TransactionController::class, 'checkout']);
+});
